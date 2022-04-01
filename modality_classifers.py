8fe1aa9822c65c6c51663f81149f13c45ae30da4
alfidhadiat <alfid@neotypewriter.localdomain>
2022-04-01 15:22:23 +0200

    Modality classifier with all 3 and just PE modalities

--- modality_classifers.py
+++ modality_classifers.py
@@ -15,14 +15,16 @@ from text_preprocessing import modality_preprocessed_dataset
 
 df = modality_preprocessed_dataset()
 
-# Change modality to binary; pe1 and pe2 into pe 
-df['modality'] = ["pe" if i != "ht" else "ht" for i in df['modality']]
+# Creating binary and sans-ht modality lists
+df['modality_2'] = ["pe" if i != "ht" else "ht" for i in df['modality']]
+pe_df = df[df['modality_2'] != 'ht']
 
 ######################
 # Logistic Regression
 ######################
 
-X = df.drop('modality', axis = 1)
+# All 3 modalities
+X = df.drop(['modality', 'modality_2'], axis = 1)
 y = df['modality']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
 
@@ -30,5 +32,37 @@ log_reg = LogisticRegression()
 log_reg.fit(X_train, y_train)
 
 pred = log_reg.predict(X_test)
-print("Logistic Regression Classifier for Modality")
+print("Alll 3 modalities")
 print(classification_report(y_test, pred))
+
+# Binary modalities
+X = df.drop(['modality', 'modality_2'], axis = 1)
+y = df['modality_2']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
+
+log_reg = LogisticRegression()
+log_reg.fit(X_train, y_train)
+
+pred = log_reg.predict(X_test)
+print("Binarized modalities")
+print(classification_report(y_test, pred))
+
+# Only PE modalities
+X = pe_df.drop(['modality', 'modality_2'], axis = 1)
+y = pe_df['modality']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
+
+log_reg = LogisticRegression()
+log_reg.fit(X_train, y_train)
+
+pred = log_reg.predict(X_test)
+print("PE modalities")
+print(classification_report(y_test, pred))
+
+###########################
+# Use informative features
+###########################
+
+#from informative_features import plot_coefficients
+#
+#plot_coefficients(log_reg, X.columns, top_features = 20)
