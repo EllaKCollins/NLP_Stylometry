0254a1ee2eeb2060c1bce5f3c0020349b71198a3
EllaKCollins <e.k.collins@student.rug.nl>
2022-04-08 13:23:31 +0200

    pos counts

--- convert_pos_counts.py
+++ convert_pos_counts.py
@@ -3,14 +3,14 @@ from collections import Counter
 
 def convert_pos_to_counts(dataframe):
 
-    columns = ['item_id', 'ADJ', 'ADP', 'ADV', 'AUX', 'CONJ', 'CCONJ', 'DET', 'INTJ', 'NOUN', 'NUM', 'PART', 'PRON', 'PROPN', 'PUNCT', 'SCONJ', 'SYM', 'VERB', 'X', 'EOL', 'SPACE']
+    columns = ['item_id', 'subject_id',	'modality',  'ADJ', 'ADP', 'ADV', 'AUX', 'CONJ', 'CCONJ', 'DET', 'INTJ', 'NOUN', 'NUM', 'PART', 'PRON', 'PROPN', 'PUNCT', 'SCONJ', 'SYM', 'VERB', 'X', 'EOL', 'SPACE']
     pos_dataframe = pd.DataFrame(columns = columns)
 
     for i, datapoint in dataframe.iterrows():
         if datapoint["aligned_edit"] != "nan":
             pos = datapoint["sub_POS"] + datapoint["del_POS"] + datapoint["in_POS"]
             pos_dict = Counter(pos)
-            array = [datapoint["item_id"]] + [0] * 20
+            array = [datapoint["item_id"], datapoint["subject_id"], datapoint["modality"]] + [0] * 20
             for key in pos_dict:
                 array[columns.index(key)] = pos_dict[key]
             pos_dataframe.loc[len(pos_dataframe)] = array
